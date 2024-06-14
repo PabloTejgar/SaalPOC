4e30e85a836e7aa94eb517ab904d43391d8161d4
PabloTejGar <pablo.tejgar@gmail.com>
2024-06-14 22:55:58 +0200

    feat: Add restaurant controller and basic restaurant model.

---
+++ Saal.API/Controllers/RestaurantController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Saal.API.Models;
+
+namespace Saal.API.Controllers
+{
+    public class RestaurantController : Controller
+    {
+        /// <summary>
+        /// Logger instance.
+        /// </summary>
+        private readonly ILogger<RestaurantController> _logger;
+
+        /// <summary>
+        /// Restaurant controller constructor.
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="logger">Logger instance.</param>
+        public RestaurantController(ILogger<RestaurantController> logger)
+        {
+            _logger = logger;
+        }
+
+        /// <summary>
+        /// Get restaurant by the id.
+        /// </summary>
+        /// <param name="code">Restaurant code to get.</param>
+        /// <response code="200">OK. Returns a restaurant.</response>        
+        /// <response code="404">Not Found. That restaurant doesn't exist.</response>              
+        [HttpGet("{code}")]
+        [ProducesResponseType(typeof(Restaurant), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Saal.API/Models/Restaurant.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Saal.API.Models
+{
+    /// <summary>
+    /// Model class restaurant.
+    /// </summary>
+    public class Restaurant
+    {
+        /// <summary>
+        /// Id key.
+        /// </summary>
+        [Key]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Name field.
+        /// </summary>
+        [Required]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// City field.
+        /// </summary>
+        [Required]
+        public string City { get; set; }
+
+
+    }
+}
